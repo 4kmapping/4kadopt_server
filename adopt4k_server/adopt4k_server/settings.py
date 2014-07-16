d1bacf025b70c5f4d99b359f934faabffffa01e5
Min Seong Kang <spiritbanner@gmail.com>
2014-07-16 11:18:54 -1000

    Commit db chage.

--- adopt4k_server/adopt4k_server/settings.py
+++ adopt4k_server/adopt4k_server/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'adoptdb',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'JJ@ng!',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
