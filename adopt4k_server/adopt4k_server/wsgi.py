af776f5c6d3c7e3d53b424cb1511740ccd05efb8
Min Seong Kang <spiritbanner@gmail.com>
2014-07-16 14:55:41 -1000

    hey

--- adopt4k_server/adopt4k_server/wsgi.py
+++ adopt4k_server/adopt4k_server/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adopt4k_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adopt4k_server.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
