3b7db5a18e0f880c521b76d22869944efafa3e76
Joshua de Lange <joshua.delange@gmail.com>
2014-08-06 13:02:09 +0200

    takes off options for now

--- adopt4k_server/STATIC/scripts/main.js
+++ adopt4k_server/STATIC/scripts/main.js
@@ -2,6 +2,5 @@ $(document).ready(function(){
 
   //globals because i want to be able to acess from console. :D
   window.ozController = ozController.init() ;
-  window.optionsController = optionsController.init() ;
 
 });
